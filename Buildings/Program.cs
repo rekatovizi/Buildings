1970145db0655f4f56babf8cdca50d6d26ef5201
Réka Tóvizi <tovizi.reka@petrik.hu>
2026-03-03 09:30:49 +0100

    Koczkaora vege

--- Buildings/Program.cs
+++ Buildings/Program.cs
@@ -6,6 +6,7 @@
         {
             List<Building> buildings = new List<Building>();
             buildings.Add(new ResidentialBuilding("Lakóház1", 5, 10));
+            Console.WriteLine(buildings);
         }
     }
 }
