2737463fcaa1752f4922de647cbd464aa03f2569
Diák <vs@petrik.hu>
2026-03-27 09:22:47 +0100

    added courthouse

---
+++ Buildings/CourtBuilding.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buildings
+{
+	internal class CourtBuilding : ServiceBuilding
+	{
+		public CourtBuilding(int x, int y) : base("Court", x, y)
+		{
+			BuildingCost = 25000;
+			MaintenanceCost = 2000;
+			Capacity = 200;
+			HappinessImpact = 10;
+			ElectricityConsumption = 40;
+			WaterConsumption = 30;
+		}
+	}
+}
