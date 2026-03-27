57ddb934c9d7720c7bdfed504283df1b443775cd
Diák <vs@petrik.hu>
2026-03-27 09:22:11 +0100

    Added Cityhall

---
+++ Buildings/CityHallBuilding.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buildings
+{
+	internal class CityHallBuilding : ServiceBuilding
+	{
+		public CityHallBuilding(int x, int y) : base("City Hall", x, y)
+		{
+			BuildingCost = 50000;
+			MaintenanceCost = 2500;
+			Capacity = 50;
+			TaxIncome = 0;
+			HappinessImpact = 40;
+			ElectricityConsumption = 35;
+			WaterConsumption = 30;
+		}
+	}
+}
