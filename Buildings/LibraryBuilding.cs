cca6fd94793ba6c2c97838dc2b5850f35f3a365f
Diák <vs@petrik.hu>
2026-03-27 09:20:45 +0100

    Added library

---
+++ Buildings/LibraryBuilding.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buildings
+{
+	internal class LibraryBuilding : ServiceBuilding
+	{
+		public LibraryBuilding(int x, int y) : base("Library", x, y)
+		{
+			BuildingCost = 15000;
+			MaintenanceCost = 1000;
+			Capacity = 75;
+			TaxIncome = 0;
+			HappinessImpact = 40;
+			ElectricityConsumption = 25;
+			WaterConsumption = 10;
+		}
+	}
+}
