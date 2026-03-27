c386552765f8c00e5f3f59a041403303cb348283
Diák <vs@petrik.hu>
2026-03-27 09:22:55 +0100

    added postoffice

---
+++ Buildings/PostOfficeBuilding.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buildings
+{
+	internal class PostOfficeBuilding : ServiceBuilding
+	{
+		public PostOfficeBuilding(int x, int y) : base("Post Office", x, y)
+		{
+			BuildingCost = 10000;
+			MaintenanceCost = 900;
+			Capacity = 60;
+			TaxIncome = 0;
+			HappinessImpact = 10;
+			ElectricityConsumption = 15;
+			WaterConsumption = 10;
+		}
+	}
+}
