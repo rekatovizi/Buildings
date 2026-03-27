7351d7d95629435b945fc9d7d2130d329dcb523c
Diák <vs@petrik.hu>
2026-03-27 09:28:06 +0100

    Changed zetenyMiatt so we can test and add other things not just ResidentialBuilding

--- Buildings/BuildingManager.cs
+++ Buildings/BuildingManager.cs
@@ -55,7 +55,7 @@
 					break;
 			}
 		}
-		public void zetenyMiatt(Type type) //Szabó Zétény miatt van ez a metódus, hogy ne kelljen minden switch case-ben ugyanazt a kódot írni
+	/*public void zetenyMiatt(Type type) //Szabó Zétény miatt van ez a metódus, hogy ne kelljen minden switch case-ben ugyanazt a kódot írni
 		{
 			Console.WriteLine("-------------------------------------------");
 			int input = int.Parse(Console.ReadLine());
@@ -65,6 +65,67 @@
 			int YPosition = int.Parse(Console.ReadLine());
 			buildingsBuilt.Add($"__{buildingsBuilt.Count + 1}", new ResidentialBuilding(Enum.GetName(type, input - 1), XPosition, YPosition));
 			Console.WriteLine("Sikerült :D");
+		}*/
+	public void zetenyMiatt(Type type)
+		{
+			Console.WriteLine("-------------------------------------------");
+			int input = int.Parse(Console.ReadLine());
+
+			string name = Enum.GetName(type, input - 1);
+
+			Console.WriteLine($"Kiválasztottad a {name} épületet.");
+			Console.WriteLine("Melyik X és Y értékre szeretnéd ülteti: ");
+
+			int x = int.Parse(Console.ReadLine());
+			int y = int.Parse(Console.ReadLine());
+
+			Building b;
+
+			if (type == typeof(Residential))
+				b = new ResidentialBuilding(name, x, y);
+
+			else if (type == typeof(Commercial))
+				b = new CommercialBuilding(name, x, y);
+
+			else if (type == typeof(Industrial))
+				b = new IndustrialBuilding(name, x, y);
+
+			else if (type == typeof(Utility))
+				b = new UtilityBuilding(name, x, y);
+
+			else if (type == typeof(Service))
+			{
+				switch ((Service)(input - 1))
+				{
+					case Service.School: b = new SchoolBuilding(x, y); break;
+					case Service.University: b = new UniversityBuilding(x, y); break;
+					case Service.Hospital: b = new Hospital(x, y); break;
+					case Service.PoliceStation: b = new PoliceStationBuilding(x, y); break;
+					case Service.FireStation: b = new FireStationBuilding(x, y); break;
+					case Service.PostOffice: b = new PostOfficeBuilding(x, y); break;
+					case Service.Library: b = new LibraryBuilding(x, y); break;
+					case Service.CityHall: b = new CityHallBuilding(x, y); break;
+					case Service.Court: b = new CourtBuilding(x, y); break;
+					case Service.Uszoda: b = new UszodaBuilding(x, y); break;
+
+					default: b = new ServiceBuilding(name, x, y); break;
+				}
+			}
+			else
+			{
+				throw new Exception("Ismeretlen típus");
+			}
+
+			// after successfully adding a building
+			if (Building.Add(b))
+			{
+				Console.WriteLine("Sikerült :D");
+				Building.Draw(); // <-- call Draw here to see the updated grid immediately
+			}
+			else
+			{
+				Console.WriteLine("Nem sikerült (foglalt hely)");
+			}
 		}
 	}
 }
